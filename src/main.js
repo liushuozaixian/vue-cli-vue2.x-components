014625b95aa821a5b76ef640ed8e242403cfbb9c
liushuo <liushuozaixian@163.com>
2018-06-19 13:47:20 +0800

    混入

--- src/main.js
+++ src/main.js
@@ -10,6 +10,26 @@ import camelCase from 'lodash/camelCase'
 import router from './router';
 import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
+
+// 定义一个混入对象
+var myMixin = {
+  created: function () {
+    this.hello()
+  },
+  methods: {
+    hello: function () {
+      console.log('hello from mixin!')
+    }
+  }
+}
+
+// 定义一个使用混入对象的组件
+var Component = Vue.extend({
+  mixins: [myMixin]
+})
+
+var component = new Component() // => "hello from mixin!"
+
 Vue.use(ElementUI);
 
 Vue.config.productionTip = false;
